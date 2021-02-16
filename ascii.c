8361cad15c868b319a0b49f5a520d8d938ad518b
Madhuri <60868865+learn-Programs@users.noreply.github.com>
2021-02-16 12:29:37 +0530

    print ascii value of entered characters.

---
+++ ascii.c
@@ -0,0 +1,14 @@
+// C program to find ASCII value of a character entered by the user.
+
+#include<stdio.h>
+
+int main(){
+
+    char c;
+
+    printf("enter a character \n");
+    scanf("%c", &c);
+
+    printf("ASCII value of character you enter is %d \n", c);
+    return 0;
+}
