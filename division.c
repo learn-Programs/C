af9708a9f3a8d866e7ab0410fda5dc5267b2d3eb
Madhuri <60868865+learn-Programs@users.noreply.github.com>
2021-02-16 12:19:53 +0530

    find quotient and remainder of two numbers

---
+++ division.c
@@ -0,0 +1,24 @@
+// c program to find quotient and remainder of two integers.
+
+#include<stdio.h>
+
+int main() {
+    int a, b;
+    int q, re;
+
+    printf("Enter dividend: ");
+    scanf("%d", &a);
+    printf("enter divisor: ");
+    scanf("%d", &b);
+
+    // computes quotient
+    q  = a/b;
+
+    // computes remainder.
+    re = a%b;
+
+    printf("Quotient = %d\n",q);
+    printf("Remainder = %d\n", re);
+    return 0;
+
+}
